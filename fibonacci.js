1dc4680563dd0e21e51761f80ec7f7d7777f92ce
Jamie Mancini <jamiemancini@gmail.com>
2022-09-26 19:09:56 -0700

    working on fibonacci recursive algorithm

---
+++ fibonacci.js
@@ -0,0 +1,13 @@
+//Fibonacci Recursive Function written in Javascript
+
+function fibonacciRecursion (n) {
+    if (n === 0) {total = total + 0
+    };
+    
+    if (n === 1) {total = total + 1
+    };
+
+    if ( n > 1) { total = fibonaacciRecursion(n-1) + total
+
+    };
+}
